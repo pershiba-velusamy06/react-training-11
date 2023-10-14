65e39734a73e957ed5b7a2135ef207bb328048c7
pershiba-velusamy06 <pershiba@elred.io>
2023-10-14 13:56:25 +0530

    session 26

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import {
   RouterProvider,
   createBrowserRouter,
 } from 'react-router-dom';
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 
 import Events from './components/Events/Events.jsx';
 import EventDetails from './components/Events/EventDetails.jsx';
 import NewEvent from './components/Events/NewEvent.jsx';
-import EditEvent from './components/Events/EditEvent.jsx';
+import EditEvent, {
+  loader as editEventLoader,
+  action as editEventAction,
+} from './components/Events/EditEvent.jsx';
+import { queryClient } from './util/http.js';
 
 const router = createBrowserRouter([
   {
@@ -18,7 +22,6 @@ const router = createBrowserRouter([
   {
     path: '/events',
     element: <Events />,
-
     children: [
       {
         path: '/events/new',
@@ -33,12 +36,12 @@ const router = createBrowserRouter([
       {
         path: '/events/:id/edit',
         element: <EditEvent />,
+        loader: editEventLoader,
+        action: editEventAction
       },
     ],
   },
 ]);
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
--- src/util/http.js
+++ src/util/http.js
@@ -1,5 +1,19 @@
-export async function fetchEvents() {
-  const response = await fetch('http://localhost:3000/events');
+import { QueryClient } from '@tanstack/react-query';
+
+export const queryClient = new QueryClient();
+
+export async function fetchEvents({ signal, searchTerm, max }) {
+  let url = 'http://localhost:8081events';
+
+  if (searchTerm && max) {
+    url += '?search=' + searchTerm + '&max=' + max;
+  } else if (searchTerm) {
+    url += '?search=' + searchTerm;
+  } else if (max) {
+    url += '?max=' + max
+  }
+
+  const response = await fetch(url, { signal: signal });
 
   if (!response.ok) {
     const error = new Error('An error occurred while fetching the events');
@@ -11,4 +25,91 @@ export async function fetchEvents() {
   const { events } = await response.json();
 
   return events;
+}
+
+
+export async function createNewEvent(eventData) {
+  const response = await fetch(`http://localhost:8081events`, {
+    method: 'POST',
+    body: JSON.stringify(eventData),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while creating the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
+}
+
+export async function fetchSelectableImages({ signal }) {
+  const response = await fetch(`http://localhost:8081events/images`, { signal });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while fetching the images');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { images } = await response.json();
+
+  return images;
+}
+
+export async function fetchEvent({ id, signal }) {
+  const response = await fetch(`http://localhost:8081events/${id}`, { signal });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while fetching the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
+}
+
+
+export async function deleteEvent({ id }) {
+  const response = await fetch(`http://localhost:8081events/${id}`, {
+    method: 'DELETE',
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while deleting the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  return response.json();
+}
+
+export async function updateEvent({ id, event }) {
+  const response = await fetch(`http://localhost:8081events/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({ event }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while updating the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  return response.json();
 }
